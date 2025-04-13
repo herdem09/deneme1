bcb028f0b5cd0440e91a667debf0a7f4a832a69c
herdem09 <herdem09@proton.me>
2025-04-14 01:30:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# app.py
+import os
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -72,4 +74,5 @@ def delete_data(key):
         return jsonify({"error": f"'{key}' bulunamadı"}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
